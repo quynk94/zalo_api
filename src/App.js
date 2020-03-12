0d6dd69d9da006af98398ca49e18e10d1eed5e91
quynk <quynk94@gmail.com>
2020-03-12 22:11:34 +0700

    Add

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { withAlert } from 'react-alert'
 
 const TOKEN = "Vh5IIDi-mHzyiL98t3_47tkxLatOARTR2Drq2_4hbbvUkc5fZbMAHLk_V6gCJF5DBfWBQO4KapDdu6bWXXk5R7NCJ4sF6A9lLP5JRuvfgcP-XMqpaapr4Yc-8ZZOJiHj6RS2NCa-cWSNqGaBr5_B016y2XB4T9j96xjSRU9WgLb4aL1SwqMsL2s1P7xaJQ9-AuH0MkTYXN41cc9D_M28SGElOaQWFg4MVjXpC94sjnv6_YuJumFdHIVATXx01E0vO-qD4xe9d48Tsmzk_GIUMfQye6FR8yOQ"
 const SEND_MESSAGE_URL = `https://openapi.zalo.me/v2.0/oa/message?access_token=${TOKEN}`
 const GET_UID_URL = `https://openapi.zalo.me/v2.0/oa/getprofile?access_token=${TOKEN}`
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       message: '',
       sdt: '',
       isSending: false
     }
-
   }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div className="App">
         <div className="app-container">
+          <div className="title">
+            Send message to Zalo user
+          </div>
           <div className="field">
             <label>Message:</label>
-            <input onChange={this._onMessageChange}></input>
+            <textarea onChange={this._onMessageChange}></textarea>
           </div>
           <div className="field">
             <label>SDT:</label>
-            <input onChange={this._onSDTChange}></input>
+            <textarea onChange={this._onSDTChange} placeholder="EX: 0123456789;012345689"></textarea>
           </div>
-          <button onClick={this._onSendClick}>Send Message</button>
+          <div className="button-container">
+            <button onClick={this._onSendClick}>Send Message</button>
+          </div>
         </div>
       </div>
     )
@@ -44,15 +52,34 @@ class App extends Component {
 
   _onSendClick = () => {
     this.setState({ isSending: true })
-    this._parseAllPhoneNumber()
+    const sdts = this._parseAllPhoneNumber()
+    sdts.forEach((sdt) => {
+      if (this._isValidPhone(sdt)) {
+        this._sendToAPhone(sdt);
+      } else {
+        this.props.alert.error(`SDT: ${sdt} không hợp lệ`)
+      }
+    })
+  }
+
+  _isValidPhone(sdt) {
+    if (/^\d{10,11}$/.test(sdt)) {
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
   _parseAllPhoneNumber() {
-    let sdts = this.state['sdt'].split(';')
-    sdts.forEach((sdt) => {
-      this._sendToAPhone(sdt);
+    let sdts = this.state['sdt'].split(';').map((sdt) => {
+      if (!sdt.startsWith('0')) {
+        sdt = '0' + sdt;
+      }
+      sdt = sdt.replace(/\s+/, '')
+      return sdt;
     })
+    return sdts;
   }
 
   _sendToAPhone(phone) {
@@ -60,11 +87,15 @@ class App extends Component {
     fetch(url).then((response) => {
       return response.json()
     })
-    .then((response) => {
-      if (response['message'] == 'Success') {
-        this._sendToAUserID(response['data']['user_id']);
-      }
-    })
+      .then((response) => {
+        if (response['message'] == 'Success') {
+          this._sendToAUserID(response['data']['user_id']).then((response) => {
+            if (response.message == "Success") {
+              this.props.alert.show(`Message sended to: ${phone}`)
+            }
+          });
+        }
+      })
 
   }
 
@@ -83,8 +114,10 @@ class App extends Component {
         'Content-Type': 'application/json;charset=utf-8'
       },
       body: JSON.stringify(data)
-    });
+    }).then((response) => (response.json()))
+
+    return response;
   }
 }
 
-export default App;
+export default withAlert()(App);
---
+++ src/Root.js
@@ -0,0 +1,23 @@
+// index.js
+import React from 'react'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+import App from './App'
+
+// optional cofiguration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
+    <App />
+  </AlertProvider>
+)
+
+export default Root;
