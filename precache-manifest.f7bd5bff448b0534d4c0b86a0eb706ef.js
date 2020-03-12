05c4bc70d2616202de2eb6064608ee05d7ca3238
quynk <quynk94@gmail.com>
2020-03-12 22:15:21 +0700

    Updates

---
+++ precache-manifest.f7bd5bff448b0534d4c0b86a0eb706ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59f93ef1b0a19830b4483c4a547d2d1",
+    "url": "/zalo_api/index.html"
+  },
+  {
+    "revision": "59554a009989078721b4",
+    "url": "/zalo_api/static/css/main.0ffa8d9a.chunk.css"
+  },
+  {
+    "revision": "484f9cd6ff263776c7f7",
+    "url": "/zalo_api/static/js/2.26856f2d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/zalo_api/static/js/2.26856f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59554a009989078721b4",
+    "url": "/zalo_api/static/js/main.e112d0a1.chunk.js"
+  },
+  {
+    "revision": "36f6722b4b37d41652ff",
+    "url": "/zalo_api/static/js/runtime-main.a2042a1d.js"
+  }
+]);
